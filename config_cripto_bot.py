368f5b29b59365a94880d2a41d19876386611f8a
BasyrovaIA <119829008+BasyrovaIA@users.noreply.github.com>
2023-06-06 11:04:37 +0500

    Create config_cripto_bot.py

---
+++ config_cripto_bot.py
@@ -0,0 +1,15 @@
+
+TOKEN = '6117257054:AAHlfx14W9_qHI_F3ffvkIrjzuCWbkuzDvQ'
+
+keys = {
+    'биткоин': 'BTC',
+    'эфириум': 'ETC',
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'рубль': 'RUB',
+    'каспа': 'KAS',
+    'юсдт': 'USDT',
+    'KASPA': 'KAS',
+    'bitcoin': 'BTC',
+    'тезер': 'USDT',
+        }
